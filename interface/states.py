0848a6a4a94b0b8beff61d648ff4fbf97bdba043
AS1 <arseniykofanov@gmail.com>
2025-09-24 12:53:48 +0300

    Добавление класса GeneralStates для управления состояниями в интерфейсе

---
+++ interface/states.py
@@ -0,0 +1,14 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class GeneralStates(StatesGroup):
+    zero = State() # Состояние по умолчанию
+    ChooseDino = State() # Состояние для выбора динозавра
+    ChooseInt = State() # Состояние для ввода числа
+    ChooseString = State() # Состояние для ввода текста
+    ChooseConfirm = State() # Состояние для подтверждения (да / нет)
+    ChooseOption = State() # Состояние для выбора среди вариантов
+    ChooseInline = State() # Состояние для выбора кнопки
+    ChoosePagesState = State() # Состояние для выбора среди вариантов, а так же поддерживает страницы
+    ChooseCustom = State() # Состояние для кастомного обработчика
+    ChooseTime = State() # Состояние для ввода времени
+    ChooseImage = State() # Состояние для ввода загрузки изображения
